4a68513afbfd044ca2036c63d9d6ad9ac674d7d6
Antonio Andre <antonioandre1008@gmail.com>
2024-02-27 14:08:37 -0500

    feat: implementa tela de login

--- components/user-auth-login.tsx
+++ components/user-auth-login.tsx
@@ -2,11 +2,15 @@
 
 import { useState } from "react";
 import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Input } from "./ui/input";
+import Link from "next/link";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -20,6 +24,8 @@ export default function UserAuthLogin({ className, ...props }: UserAuthFormProps
         email: "",
         password: "",
     });
+
+    const router = useRouter();
 
     const searchParams = useSearchParams();
     const search = searchParams.get("error");
@@ -46,51 +52,66 @@ export default function UserAuthLogin({ className, ...props }: UserAuthFormProps
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <div className="grid gap-2">
-                    <div className="grid gap-1">
-                        <Label htmlFor="email" className="sr-only">
-                            Email
-                        </Label>
-                        <Input
-                            disabled={isDisabled}
-                            id="email"
-                            name="email"
-                            placeholder="email@gmail.com"
-                            type="text"
-                            autoCapitalize="none"
-                            autoComplete="none"
-                            autoCorrect="off"
-                            value={formData.email}
-                            onChange={handleFormChange}
-                        />
-                    </div>
-                    <div className="grid gap-1">
-                        <Label htmlFor="username" className="sr-only">
-                            Password
-                        </Label>
-                        <Input
-                            disabled={isDisabled}
-                            id="password"
-                            name="password"
-                            placeholder="paSsw0rd!"
-                            type="password"
-                            autoCapitalize="none"
-                            autoComplete="none"
-                            autoCorrect="off"
-                            value={formData.password}
-                            onChange={handleFormChange}
-                        />
-                    </div>
-                    <div className="grid gap-1">
-                        {search === "CredentialsSignin" && (
-                            <p className="text-red-500 text-sm">Credenciais inválidas, tente novamente.</p>
-                        )}
-                    </div>
-                    <Button disabled={isDisabled}>
-                        {isDisabled && <Loader2 className="animate-spin" />}
-                        {!isDisabled && "Entrar"}
-                    </Button>
-                </div>
+                <Tabs defaultValue="login" className="w-[400px]">
+                    <TabsList className="w-full">
+                        <TabsTrigger className="w-full" value="login">
+                            Login
+                        </TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="login">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Entrar na Conta</CardTitle>
+                                <CardDescription>
+                                    Se você ainda não possui uma conta, crie uma preenchendo as informações abaixo.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent className="space-y-2">
+                                <div className="space-y-1">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input
+                                        data-testid="email"
+                                        id="email"
+                                        name="email"
+                                        placeholder="exemplo@gmail.com"
+                                        type="text"
+                                        autoCapitalize="none"
+                                        autoComplete="none"
+                                        autoCorrect="off"
+                                        value={formData.email}
+                                        onChange={handleFormChange}
+                                    />
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="password">Senha</Label>
+                                    <Input
+                                        data-testid="password"
+                                        id="password"
+                                        name="password"
+                                        placeholder="p4ssw0rd!"
+                                        type="text"
+                                        autoCapitalize="none"
+                                        autoComplete="none"
+                                        autoCorrect="off"
+                                        value={formData.password}
+                                        onChange={handleFormChange}
+                                    />
+                                </div>
+                                {search === "CredentialsSignin" && (
+                                    <p className="text-red-500 text-sm italic">
+                                        Credenciais inválidas, por favor tente novamente.
+                                    </p>
+                                )}
+                            </CardContent>
+                            <CardFooter>
+                                <Button className="w-full text-center" disabled={isDisabled}>
+                                    {isDisabled && <Loader2 className="animate-spin" />}
+                                    {!isDisabled && "Entrar"}
+                                </Button>
+                            </CardFooter>
+                        </Card>
+                    </TabsContent>
+                </Tabs>
             </form>
         </div>
     );
