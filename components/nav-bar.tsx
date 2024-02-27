43481124b8406f1a35dc0aa05212526c9ef45a05
Antonio Andre <antonioandre1008@gmail.com>
2024-02-27 11:24:56 -0500

    style: estiliza header

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -8,8 +8,13 @@ const NavBar = async () => {
     return (
         <header className="flex justify-between items-center px-24 py-2 sticky top-0 z-50 border-b bg-background/95 backdrop-blur ">
             {session ? "" : <h1 className="text-2xl">Para continuar faça login</h1>}
-            <div>
-                <Button>Fazer login</Button>
+            <div className="flex gap-4 items-baseline">
+                <Button variant={"default"} size={"lg"} className="font-semibold text-xl">
+                    Fazer login
+                </Button>
+                <Button variant={"secondary"} size={"default"} className="font-medium text-xl">
+                    Criar conta
+                </Button>
                 <ToggleTheme />
             </div>
         </header>
