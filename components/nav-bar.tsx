cea93a9131fde6c829e0422dfcda28c46cadbc59
Antonio Andre <antonioandre1008@gmail.com>
2024-02-27 11:33:55 -0500

    feat: inclui icone de verificação

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -2,12 +2,17 @@ import { getServerSession } from "next-auth";
 import { auth as authOptions } from "@/lib/auth-config";
 import { ToggleTheme } from "@/components/toggle-theme";
 import { Button } from "@/components/ui/button";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
 
 const NavBar = async () => {
     const session = await getServerSession(authOptions);
     return (
         <header className="flex justify-between items-center px-24 py-2 sticky top-0 z-50 border-b bg-background/95 backdrop-blur ">
-            {session ? "" : <h1 className="text-2xl">Para continuar faça login</h1>}
+            {session ? (
+                <ShieldCheck className={`${session && "text-emerald-500"}`} size={"32"} />
+            ) : (
+                <ShieldAlert className={`${!session && "text-red-500"}`} size={"32"} />
+            )}
             <div className="flex gap-4 items-baseline">
                 <Button variant={"default"} size={"lg"} className="font-semibold text-xl">
                     Fazer login
