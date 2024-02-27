4d34ef3e84ad18492cd9e8f52e02bb1b91747198
Antonio Andre <antonioandre1008@gmail.com>
2024-02-27 10:39:06 -0500

    feat: inclui toggle theme no header

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,11 +1,12 @@
 import { getServerSession } from "next-auth";
 import { auth as authOptions } from "@/lib/auth-config";
+import { ToggleTheme } from "@/components/toggle-theme";
 
 const NavBar = async () => {
     const session = await getServerSession(authOptions);
     return (
         <header>
-            <nav>oi</nav>
+            <ToggleTheme />
         </header>
     );
 };
