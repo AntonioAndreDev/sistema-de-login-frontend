494c8cbd80d5231cb68eb6e81534ad5dfc650c03
Antonio Andre <antonioandre1008@gmail.com>
2024-02-27 20:13:09 -0500

    style: usa button de shadcn em signout

--- components/user-auth-singout.tsx
+++ components/user-auth-singout.tsx
@@ -1,15 +1,17 @@
 "use client";
 import { signOut } from "next-auth/react";
+import { Button } from "@/components/ui/button";
 
 export default function UserAuthSignout() {
     return (
-        <button
-            className="bg-red-600 px-6 py-2 w-full text-xl flex items-center justify-center font-semibold"
+        <Button
+            variant={"destructive"}
+            className="px-6 py-2 w-full text-xl flex items-center justify-center font-semibold"
             onClick={() => {
                 signOut({ callbackUrl: "/" });
             }}
         >
             Sair
-        </button>
+        </Button>
     );
 }
