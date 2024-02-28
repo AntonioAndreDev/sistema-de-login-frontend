d7597b7bb9fe96d2fd799dab862519bef4732453
Antonio Andre <antonioandre1008@gmail.com>
2024-02-27 20:05:09 -0500

    refactor: corrige local de criação do provider theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import AuthProvider from "@/components/providers/auth-provider";
 import NextTopLoader from "nextjs-toploader";
 import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
