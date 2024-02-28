0c5d5e0452646fd57167e0b8fd6fa6a8304d6468
Antonio Andre <antonioandre1008@gmail.com>
2024-02-28 10:40:21 -0500

    feat: bloqueia acesso para unauthorized se for admin

--- app/unauthorized/page.tsx
+++ app/unauthorized/page.tsx
@@ -1,4 +1,14 @@
+import { getServerSession } from "next-auth";
+import { auth as authOptions } from "@/lib/auth-config";
+import { redirect } from "next/navigation";
+
 export default async function UnauthorizedPage() {
+    const session = await getServerSession(authOptions);
+
+    if (session?.user.role === "admin") {
+        return redirect("/admin-only");
+    }
+
     return (
         <div className="text-center text-6xl text-green-700">
             <span>You are not authorized!</span>
