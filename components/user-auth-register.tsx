c828f58ecea1ac2a92b4511090ba6bbb554bc803
Antonio Andre <antonioandre1008@gmail.com>
2024-03-01 10:20:41 -0500

    fix: corrige lógica de criar email duplicado

--- components/user-auth-register.tsx
+++ components/user-auth-register.tsx
@@ -15,183 +15,181 @@ import Link from "next/link";
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 interface IUser {
-    name: string;
-    password: string;
-    email: string;
-    domain: string;
+  name: string;
+  password: string;
+  email: string;
+  domain: string;
 }
 
 export default function UserAuthRegister({ className, ...props }: UserAuthFormProps) {
-    const [formData, setFormData] = useState<IUser>({
-        name: "",
-        password: "",
-        email: "",
-        domain: "",
-    });
-    const [isLoading, setIsLoading] = useState(false);
-    const { toast } = useToast();
-    const router = useRouter();
+  const [formData, setFormData] = useState<IUser>({
+    name: "",
+    password: "",
+    email: "",
+    domain: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useToast();
+  const router = useRouter();
 
-    function handleFormChange(ev: React.ChangeEvent<HTMLInputElement>) {
-        setFormData((prev) => {
-            return { ...prev, [ev.target.name]: ev.target.value };
-        });
+  function handleFormChange(ev: React.ChangeEvent<HTMLInputElement>) {
+    setFormData((prev) => {
+      return { ...prev, [ev.target.name]: ev.target.value };
+    });
+  }
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    // Validate form data
+    if (!formData.email || !formData.name || !formData.password) {
+      setIsLoading(false);
+      toast({
+        title: "Preencha todos os campos!",
+        variant: "destructive",
+        description: "Preencha todos os campos para continuar.",
+      });
+      return;
     }
 
-    async function onSubmit(event: React.SyntheticEvent) {
-        event.preventDefault();
-        setIsLoading(true);
+    const res = await fetch("api/users", {
+      method: "POST",
+      body: JSON.stringify(formData),
+    });
 
-        // Validate form data
-        if (!formData.email || !formData.name || !formData.password) {
-            setIsLoading(false);
-            toast({
-                title: "Preencha todos os campos!",
-                variant: "destructive",
-                description: "Preencha todos os campos para continuar.",
-            });
-            return;
-        }
+    if (!res.ok) {
+      const errorData = await res.json();
+      console.log(errorData.error);
 
-        const res = await fetch("api/users", {
-            method: "POST",
-            body: JSON.stringify(formData),
-        });
-
-        if (!res.ok) {
-            const errorData = await res.json();
-            console.log(errorData.error);
-
-            if (errorData.error === "Email já cadastrado") {
-                setIsLoading(false);
-                toast({
-                    title: "Email já cadastrado!",
-                    variant: "destructive",
-                    description: "Entre na sua conta ou faça um novo cadastro.",
-                });
-                return;
-            }
-
-            if (errorData.error === "Preencha todos os campos") {
-                setIsLoading(false);
-                toast({
-                    title: "Preencha todos os campos!",
-                    variant: "destructive",
-                    description: "Preencha todos os campos para continuar.",
-                });
-                return;
-            }
-        }
-
-        setFormData({
-            name: "",
-            password: "",
-            email: "",
-            domain: "",
-        });
-
+      if (errorData.error === "Usuário já possui email cadastrado!") {
         setIsLoading(false);
         toast({
-            title: "Conta criada com sucesso!",
-            variant: "default",
-            description: "Agora você pode fazer login.",
+          title: "Email já cadastrado!",
+          variant: "destructive",
+          description: "Entre na sua conta ou faça um novo cadastro.",
         });
+        return;
+      }
 
-        router.push("/login");
+      if (errorData.error === "É necessário preencher todos os campos!") {
+        setIsLoading(false);
+        toast({
+          title: "Preencha todos os campos!",
+          variant: "destructive",
+          description: "Preencha todos os campos para continuar.",
+        });
+        return;
+      }
     }
 
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <Tabs defaultValue="register" className="w-[400px]">
-                    <TabsContent value="register">
-                        <Card>
-                            <CardHeader>
-                                <Button variant={"secondary"} asChild>
-                                    <Link href={"/login"}>Já tenho uma conta</Link>
-                                </Button>
-                            </CardHeader>
-                            <Separator />
-                            <CardHeader>
-                                <CardTitle>Criar Conta</CardTitle>
-                                <CardDescription>
-                                    Se você ainda não possui uma conta, crie uma preenchendo as informações abaixo.
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className="space-y-2">
-                                <div className="space-y-1">
-                                    <Label htmlFor="name">Nome</Label>
-                                    <Input
-                                        data-testid="name"
-                                        id="name"
-                                        name="name"
-                                        placeholder="Antonio Andre"
-                                        type="text"
-                                        autoCapitalize="none"
-                                        autoComplete="none"
-                                        autoCorrect="off"
-                                        value={formData.name
-                                            .normalize("NFD")
-                                            .replace(/[\u0300-\u036f]/g, "")
-                                            .replace(/\b\w/g, (char) => char.toUpperCase())}
-                                        onChange={handleFormChange}
-                                    />
-                                </div>
-                                <div className="space-y-1">
-                                    <Label htmlFor="email">Email</Label>
-                                    <Input
-                                        data-testid="email"
-                                        id="email"
-                                        name="email"
-                                        placeholder="exemplo@gmail.com"
-                                        type="email"
-                                        autoCapitalize="on"
-                                        value={formData.email}
-                                        onChange={handleFormChange}
-                                    />
-                                </div>
-                                <div className="space-y-1">
-                                    <Label htmlFor="password">Senha</Label>
-                                    <Input
-                                        data-testid="password"
-                                        id="password"
-                                        name="password"
-                                        placeholder="p4ssw0rd!"
-                                        type="password"
-                                        autoCapitalize="none"
-                                        autoComplete="none"
-                                        autoCorrect="off"
-                                        value={formData.password}
-                                        onChange={handleFormChange}
-                                    />
-                                </div>
-                                <div className="space-y-1">
-                                    <Label htmlFor="domain">Domínio</Label>
-                                    <Input
-                                        data-testid="domain"
-                                        id="domain"
-                                        name="domain"
-                                        placeholder="p4ssw0rd!"
-                                        type="domain"
-                                        autoCapitalize="none"
-                                        autoComplete="none"
-                                        autoCorrect="off"
-                                        value={formData.domain}
-                                        onChange={handleFormChange}
-                                    />
-                                </div>
-                            </CardContent>
-                            <CardFooter>
-                                <Button className="w-full" disabled={isLoading}>
-                                    {" "}
-                                    {isLoading && <Loader2 className="animate-spin" />}
-                                    {!isLoading && "Criar conta"}
-                                </Button>
-                            </CardFooter>
-                        </Card>
-                    </TabsContent>
-                </Tabs>
-            </form>
-        </div>
-    );
+    setFormData({
+      name: "",
+      password: "",
+      email: "",
+      domain: "",
+    });
+
+    setIsLoading(false);
+    toast({
+      title: "Conta criada com sucesso!",
+      variant: "default",
+      description: "Agora você pode fazer login.",
+    });
+
+    router.push("/login");
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <Tabs defaultValue="register" className="w-[400px]">
+          <TabsContent value="register">
+            <Card>
+              <CardHeader>
+                <Button variant={"secondary"} asChild>
+                  <Link href={"/login"}>Já tenho uma conta</Link>
+                </Button>
+              </CardHeader>
+              <Separator />
+              <CardHeader>
+                <CardTitle>Criar Conta</CardTitle>
+                <CardDescription>Se você ainda não possui uma conta, crie uma preenchendo as informações abaixo.</CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="name">Nome</Label>
+                  <Input
+                    data-testid="name"
+                    id="name"
+                    name="name"
+                    placeholder="Antonio Andre"
+                    type="text"
+                    autoCapitalize="none"
+                    autoComplete="none"
+                    autoCorrect="off"
+                    value={formData.name
+                      .normalize("NFD")
+                      .replace(/[\u0300-\u036f]/g, "")
+                      .replace(/\b\w/g, (char) => char.toUpperCase())}
+                    onChange={handleFormChange}
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    data-testid="email"
+                    id="email"
+                    name="email"
+                    placeholder="exemplo@gmail.com"
+                    type="email"
+                    autoCapitalize="on"
+                    value={formData.email}
+                    onChange={handleFormChange}
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="password">Senha</Label>
+                  <Input
+                    data-testid="password"
+                    id="password"
+                    name="password"
+                    placeholder="p4ssw0rd!"
+                    type="password"
+                    autoCapitalize="none"
+                    autoComplete="none"
+                    autoCorrect="off"
+                    value={formData.password}
+                    onChange={handleFormChange}
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="domain">Domínio</Label>
+                  <Input
+                    data-testid="domain"
+                    id="domain"
+                    name="domain"
+                    placeholder="p4ssw0rd!"
+                    type="domain"
+                    autoCapitalize="none"
+                    autoComplete="none"
+                    autoCorrect="off"
+                    value={formData.domain}
+                    onChange={handleFormChange}
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button className="w-full" disabled={isLoading}>
+                  {" "}
+                  {isLoading && <Loader2 className="animate-spin" />}
+                  {!isLoading && "Criar conta"}
+                </Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </form>
+    </div>
+  );
 }
