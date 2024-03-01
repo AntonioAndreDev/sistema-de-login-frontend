833ea95ce2c71167e6bcff06f8b112bbab96ea40
Antonio Andre <antonioandre1008@gmail.com>
2024-03-01 10:51:05 -0500

    style: altera borda do nav bar

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -8,51 +8,51 @@ import { getServerSession } from "next-auth";
 import { auth as authOptions } from "@/lib/auth-config";
 
 const NavBar = async () => {
-    const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-    return (
-        <header className="flex justify-between items-center px-24 py-2 sticky top-0 z-50 border-b bg-background/95 backdrop-blur ">
-            {session ? (
-                <div className="flex gap-4 items-center">
-                    <ShieldCheck className={`${session && "text-emerald-500"}`} size={"32"} />
-                    <h1 className="text-2xl">
-                        Olá, <span className="capitalize">{session.user.name}</span>!
-                    </h1>
-                </div>
-            ) : (
-                <div className="flex items-center gap-4">
-                    <ShieldAlert className={`${!session && "text-red-500"}`} size={"32"} />
-                </div>
-            )}
-            {session && (
-                <nav className="text-base flex gap-12">
-                    <Button className="text-lg" asChild>
-                        <Link href={"/"}>Auth - Server</Link>
-                    </Button>
-                    <Button className="text-lg" asChild>
-                        <Link href={"auth-client"}>Auth - Client</Link>
-                    </Button>
-                    <Button className="text-lg" asChild>
-                        <Link href={"/admin-only"}>Admin - Only</Link>
-                    </Button>
-                    <ChangeDomain />
-                </nav>
-            )}
-            <div className="flex gap-4 items-baseline">
-                {!session ? (
-                    <Button variant={"default"} className="font-semibold text-lg px-12" asChild>
-                        <Link href={`/login`}>Entrar</Link>
-                    </Button>
-                ) : (
-                    <Button variant={"destructive"} className="font-semibold text-lg px-12" asChild>
-                        <Link href={`/signout`}>Sair</Link>
-                    </Button>
-                )}
+  return (
+    <header className="flex justify-between items-center px-24 py-2 sticky top-0 z-50 border-b border-b-gray-500/50 bg-background/95 backdrop-blur ">
+      {session ? (
+        <div className="flex gap-4 items-center">
+          <ShieldCheck className={`${session && "text-emerald-500"}`} size={"32"} />
+          <h1 className="text-2xl">
+            Olá, <span className="capitalize">{session.user.name}</span>!
+          </h1>
+        </div>
+      ) : (
+        <div className="flex items-center gap-4">
+          <ShieldAlert className={`${!session && "text-red-500"}`} size={"32"} />
+        </div>
+      )}
+      {session && (
+        <nav className="text-base flex gap-12">
+          <Button className="text-lg" asChild>
+            <Link href={"/"}>Auth - Server</Link>
+          </Button>
+          <Button className="text-lg" asChild>
+            <Link href={"auth-client"}>Auth - Client</Link>
+          </Button>
+          <Button className="text-lg" asChild>
+            <Link href={"/admin-only"}>Admin - Only</Link>
+          </Button>
+          <ChangeDomain />
+        </nav>
+      )}
+      <div className="flex gap-4 items-baseline">
+        {!session ? (
+          <Button variant={"default"} className="font-semibold text-lg px-12" asChild>
+            <Link href={`/login`}>Entrar</Link>
+          </Button>
+        ) : (
+          <Button variant={"destructive"} className="font-semibold text-lg px-12" asChild>
+            <Link href={`/signout`}>Sair</Link>
+          </Button>
+        )}
 
-                <ToggleTheme />
-            </div>
-        </header>
-    );
+        <ToggleTheme />
+      </div>
+    </header>
+  );
 };
 
 export default NavBar;
